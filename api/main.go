e2bbac7e458be3725e51226674a06434ac3de82d
siddhant <siddhantdoshi280592@gmail.com>
2022-11-30 14:29:29 +0000

    Update main.go

--- api/main.go
+++ api/main.go
@@ -13,7 +13,7 @@ func main() {
 
 	port := routes.Port
 	// Routes for shortening and resolving the url
-	r.HandleFunc("/shorten/", routes.ShortenUrl).Methods("POST")
+	r.HandleFunc("/shorten", routes.ShortenUrl).Methods("POST")
 	r.HandleFunc("/{id}", routes.ResolveUrl).Methods("GET")
-	log.Fatal(http.ListenAndServe(":"+port, r))
+	log.Fatal(http.ListenAndServe("0.0.0.0:"+port, r))
 }
