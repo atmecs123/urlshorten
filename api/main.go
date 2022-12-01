2bcee5ac7239f5bead8f961340d6c7ddc21be068
siddhant <siddhantdoshi280592@gmail.com>
2022-12-01 14:49:44 +0530

    Update main.go

--- api/main.go
+++ api/main.go
@@ -1,19 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gorilla/mux"
+	"github.com/mitchellh/go-homedir"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 	"urlshorten/routes"
 )
 
 func main() {
 	//New router for url shortening
 	r := mux.NewRouter()
-
+	dir, _ := homedir.Dir()
+	fmt.Println("dir is", dir)
+	urlFilePath := filepath.Join(dir, "urls")
+	err := os.MkdirAll(urlFilePath, 0744)
+	if err != nil {
+		log.Fatal("Failed to create directory", err)
+	}
 	port := routes.Port
 	// Routes for shortening and resolving the url
-	r.HandleFunc("/shorten", routes.ShortenUrl).Methods("POST")
-	r.HandleFunc("/{id}", routes.ResolveUrl).Methods("GET")
+	r.Handle("/shorten", routes.ShortenUrl(urlFilePath)).Methods("POST")
+	r.Handle("/{id}", routes.ResolveUrl(urlFilePath)).Methods("GET")
 	log.Fatal(http.ListenAndServe("0.0.0.0:"+port, r))
 }
