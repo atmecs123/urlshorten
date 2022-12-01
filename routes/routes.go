23c511e6a89782a2cdee89e38e37fafa7874452c
siddhant <siddhantdoshi280592@gmail.com>
2022-12-01 14:49:00 +0530

    Update routes.go

--- routes/routes.go
+++ routes/routes.go
@@ -2,15 +2,21 @@ package routes
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/asaskevich/govalidator"
 	"github.com/google/uuid"
 	"github.com/gorilla/mux"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"net/url"
+	"os"
+	"path/filepath"
 )
 
 const (
-	Port = "9000"
+	Port    = "9000"
+	UrlFile = "urls.json"
 )
 
 type Url struct {
@@ -19,63 +25,116 @@ type Url struct {
 	ShortUrl string `json:"shortUrl"`
 }
 
-var UrlMap = make(map[string]Url)
-
 // ShortenUrl is handler to shorten the long url and in response sends id,longUrl,shortUrl
-func ShortenUrl(w http.ResponseWriter, r *http.Request) {
-	var reqUrl Url
-	err := json.NewDecoder(r.Body).Decode(&reqUrl)
-	if err != nil {
-		json.NewEncoder(w).Encode("Unable to parse the request")
-		return
-	}
-	if reqUrl.LongUrl == "" {
-		json.NewEncoder(w).Encode("Empty url in the request")
-	}
-	if !govalidator.IsURL(reqUrl.LongUrl) {
-		json.NewEncoder(w).Encode("Not a valid url")
-		return
-	}
-	_, err = url.ParseRequestURI(reqUrl.LongUrl)
-	if err != nil {
-		json.NewEncoder(w).Encode("Not a valid url")
-		return
-	}
+func ShortenUrl(urlPath string) http.Handler {
+	handleFunc := func(w http.ResponseWriter, r *http.Request) {
+		filepath := filepath.Join(urlPath, UrlFile)
+		var reqUrl Url
+		err := json.NewDecoder(r.Body).Decode(&reqUrl)
+		if err != nil {
+			json.NewEncoder(w).Encode("Unable to parse the request")
+			return
+		}
+		if reqUrl.LongUrl == "" {
+			json.NewEncoder(w).Encode("Empty url in the request")
+		}
+		if !govalidator.IsURL(reqUrl.LongUrl) {
+			json.NewEncoder(w).Encode("Not a valid url")
+			return
+		}
+		_, err = url.ParseRequestURI(reqUrl.LongUrl)
+		if err != nil {
+			json.NewEncoder(w).Encode("Not a valid url")
+			return
+		}
+		var urls []Url
+		_, err = os.Stat(filepath)
+		if err != nil {
+			log.Printf("Unable to stat a file %v creating the file path...", err)
+		}
+		// create file if not exists
+		if os.IsNotExist(err) {
+			var file, err = os.Create(filepath)
+			if err != nil {
+				log.Fatalf("Unable to create a file %v", err)
+			}
+			defer file.Close()
+		}
+		file, err := os.Stat(filepath)
+		if err != nil {
+			log.Printf("Unable to stat a file %v creating the file path...", err)
+		}
+		if file.Size() != 0 {
+			file, _ := ioutil.ReadFile(filepath)
+			_ = json.Unmarshal([]byte(file), &urls)
+			if err != nil {
+				log.Fatalf("Unable to unmarshal %v", err)
+				return
+			}
+		}
+		var respUrl Url
+		for _, url := range urls {
+			if reqUrl.LongUrl == url.LongUrl {
+				json.NewEncoder(w).Encode("Short url already exists " + url.ShortUrl)
+				return
+			}
+		}
 
-	var respUrl Url
-	shortUrl, ok := UrlMap[reqUrl.LongUrl]
-	if ok {
-		json.NewEncoder(w).Encode("Short url already exists " + shortUrl.ShortUrl)
-		return
-	} else {
 		var id string
 		id = uuid.New().String()[:6]
 		newShort := "http://localhost:" + Port + "/" + id
 		respUrl.Id = id
 		respUrl.LongUrl = reqUrl.LongUrl
 		respUrl.ShortUrl = newShort
-		UrlMap[reqUrl.LongUrl] = respUrl
+		urls = append(urls, respUrl)
+		data, err := json.MarshalIndent(urls, "", " ")
+		if err != nil {
+			log.Fatalf("Unable to marshal the urls %v", err)
+		}
+		fmt.Println("Inside else condition")
+		err = ioutil.WriteFile(filepath, data, 0644)
+		if err != nil {
+			log.Fatalf("Unable to write to the file %v", err)
+		}
 		json.NewEncoder(w).Encode(respUrl)
+
 	}
+	return http.HandlerFunc(handleFunc)
 }
 
 // ResolveUrl handler resolves the short or custom url to actual long url
-func ResolveUrl(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	id := params["id"]
-	if id == "" {
-		json.NewEncoder(w).Encode("No short url id passed")
-		return
-	}
-	var urlFound bool
-	for long, short := range UrlMap {
-		if short.Id == id {
-			urlFound = true
-			http.Redirect(w, r, long, 401)
+func ResolveUrl(urlPath string) http.Handler {
+	handleFunc := func(w http.ResponseWriter, r *http.Request) {
+		filepath := filepath.Join(urlPath, UrlFile)
+		params := mux.Vars(r)
+		id := params["id"]
+		if id == "" {
+			json.NewEncoder(w).Encode("No short url id passed")
 			return
 		}
+		var urls []Url
+		file, err := ioutil.ReadFile(filepath)
+		if err != nil {
+			log.Fatalf("Unable to read the urls file %v", err)
+			return
+		}
+		err = json.Unmarshal([]byte(file), &urls)
+		if err != nil {
+			log.Fatalf("Unable to unmarshal urls %v", err)
+			return
+		}
+
+		var urlFound bool
+		for _, url := range urls {
+			if url.Id == id {
+				urlFound = true
+				http.Redirect(w, r, url.LongUrl, 401)
+				return
+			}
+		}
+		if !urlFound {
+			json.NewEncoder(w).Encode("Invalid short url passed")
+		}
 	}
-	if !urlFound {
-		json.NewEncoder(w).Encode("Invalid short url passed")
-	}
+	return http.HandlerFunc(handleFunc)
 }
